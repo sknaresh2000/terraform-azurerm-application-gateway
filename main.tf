17640bb3d0c31f5a4c26dfb869c6b11e6891adb8
NareshKumaran Sathiaseelan <naresh@nareshkransmini.mynetworksettings.com>
2023-02-04 16:45:40 -0500

    Removed lookup function

--- main.tf
+++ main.tf
@@ -104,11 +104,11 @@ resource "azurerm_application_gateway" "app-gateway" {
     for_each = var.http_listeners
     content {
       name                           = http_listener.key
-      frontend_ip_configuration_name = lookup(http_listener.value,frontend_ip_configuration_name,null)
-      frontend_port_name             = lookup(http_listener.value,frontend_port_name,null)
-      protocol                       = lookup(http_listener.value,protocol,null)
-      ssl_certificate_name           = lookup(http_listener.value,ssl_certificate_name,null)
-      host_name                      = lookup(http_listener.value,host_name,null)
+      frontend_ip_configuration_name = http_listener.value.frontend_ip_configuration_name
+      frontend_port_name             = http_listener.value.frontend_port_name
+      protocol                       = http_listener.value.protocol
+      ssl_certificate_name           = http_listener.value.ssl_certificate_name
+      host_name                      = http_listener.value.host_name
     }
   }
 
